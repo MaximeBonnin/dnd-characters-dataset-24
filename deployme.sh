e885d9b458b8a9c84cd083fb42b7b4a109744584
MaximeBonnin <maxime.bonnin@gmx.de>
2024-02-09 20:45:57 +0100

    update deploy script

--- deployme.sh
+++ deployme.sh
@@ -1,11 +1,18 @@
 
-# I would recommend running these commands one by one but I think they should all work together (I warned you)
 
-# add your info
-MONGO_USER="" 
-MONGO_PW=""
+INSTANCE_NAME="scraper-instance"
+GCP_ZONE="us-west4-b"
+
+
+# deploy GCP VM
+
+gcloud compute instances create $INSTANCE_NAME --zone=$GCP_ZONE --machine-type=e2-medium --tags=https-server
+
 
 # SSH into VM (E2 was big enough for me)
+
+gcloud compute ssh --zone $GCP_ZONE $INSTANCE_NAME
+# go though process of creating ssh keys (enter 3 times)
 
 cd ~
 
@@ -27,6 +34,10 @@ echo "Y" | sudo apt-get install git
 # get repo
 git clone https://github.com/MaximeBonnin/dnd-characters-dataset-24.git
 
+# tmux is needed to be able to close the ssh connection without the script stopping
+# ctrl + b -> d to detach from tmux session later
+tmux 
+
 # setup venv (rename if you use another repo)
 cd ~/dnd-characters-dataset-24
 python3 -m venv env
@@ -36,12 +47,21 @@ source env/bin/activate
 pip install -r requirements.txt
 
 # setup secrets (this is still terrible and I should learn to use env variables in my script)
+# add your info
+MONGO_USER="" 
+MONGO_PW=""
+HOST=""
+
 cat << EOF > my_secrets.py
-user_name = $MONGO_USER
-password = "$MONGO_PW
+user_name = "$MONGO_USER"
+password = "$MONGO_PW"
+host = "$HOST"
 EOF
 
 # start the script
 python main.py
 
 # There is one more confirmation required here. Just press "Y" when prompted.
+
+# ctrl + b -> d to detach from tmux session
+# tmux attach to connect again
